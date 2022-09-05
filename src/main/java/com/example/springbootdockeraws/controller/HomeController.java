092df97a4b7fcdc6aa3c52f7d2b41684cac2c8f9
nhh75 <nguyenhoanghai0507@gmail.com>
2022-09-05 15:39:22 +0700

    deploy docker into aws

--- src/main/java/com/example/springbootdockeraws/controller/HomeController.java
+++ src/main/java/com/example/springbootdockeraws/controller/HomeController.java
@@ -1,11 +1,12 @@
 package com.example.springbootdockeraws.controller;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
+@ComponentScan
 public class HomeController {
-    @RequestMapping("/")
+    @RequestMapping("")
     public String index(){
         return "Nguyễn Hoàng Hải Dep Trai";
     }
